776dec8bb80b997cc8fdd01a458e05e9e20b2ff0
Christian Lange <lange@synyx.de>
2018-02-02 23:53:59 +0100

    used webjars for bootstrap

--- src/main/java/org/synyx/urlaubsverwaltung/config/WebMvcConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import org.synyx.urlaubsverwaltung.web.UserInterceptor;
@@ -25,5 +26,12 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
 
         registry.addInterceptor(userInterceptor).addPathPatterns("/web/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
     }
 }
