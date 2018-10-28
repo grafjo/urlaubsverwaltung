2a03cc96d50c6d15e2957f2c543a00de1a03febc
Daniel Fuchs <dfuchs@synyx.de>
2018-10-28 11:42:11 +0100

    refactoring for better testability
    took out a ExchangeFactory to be able to manipulate the object-creation
    
    #421

--- src/main/java/org/synyx/urlaubsverwaltung/core/sync/providers/exchange/ExchangeCalendarProvider.java
+++ src/main/java/org/synyx/urlaubsverwaltung/core/sync/providers/exchange/ExchangeCalendarProvider.java
@@ -41,6 +41,7 @@ public class ExchangeCalendarProvider implements CalendarProvider {
 
     private final MailService mailService;
     private final ExchangeService exchangeService;
+    private final ExchangeFactory exchangeFactory;
 
     private String credentialsMailAddress;
     private String credentialsPassword;
@@ -48,8 +49,15 @@ public class ExchangeCalendarProvider implements CalendarProvider {
     @Autowired
     public ExchangeCalendarProvider(MailService mailService) {
 
+        this(mailService, new ExchangeService(), new ExchangeFactory());
+    }
+
+    public ExchangeCalendarProvider(MailService mailService,
+                                    ExchangeService exchangeService, ExchangeFactory exchangeFactory) {
+
         this.mailService = mailService;
-        this.exchangeService = new ExchangeService();
+        this.exchangeService = exchangeService;
+        this.exchangeFactory = exchangeFactory;
     }
 
     @Override
@@ -62,7 +70,7 @@ public class ExchangeCalendarProvider implements CalendarProvider {
         try {
             CalendarFolder calendarFolder = findOrCreateCalendar(calendarName);
 
-            Appointment appointment = new Appointment(exchangeService);
+            Appointment appointment = this.exchangeFactory.getNewAppointment(exchangeService);
 
             fillAppointment(absence, appointment, calendarSettings.getExchangeCalendarSettings().getTimeZoneId());
 
