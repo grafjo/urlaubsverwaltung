28d1b8ba981b08303751b96f553c9ae96090eec5
Johannes Graf <graf@synyx.de>
2019-04-13 17:05:25 +0200

    introduce MailOptionProvider

---
+++ src/main/java/org/synyx/urlaubsverwaltung/mail/MailOptionProvider.java
@@ -0,0 +1,14 @@
+package org.synyx.urlaubsverwaltung.mail;
+
+public interface MailOptionProvider {
+
+    String getSender();
+    String getAdministrator();
+    String getApplicationUrl();
+    Integer getMailServerPort();
+    String getMailServerHost();
+
+    default String formatApplicationUrl(String applicationurl) {
+        return applicationurl.endsWith("/")? applicationurl : applicationurl + "/";
+    }
+}
