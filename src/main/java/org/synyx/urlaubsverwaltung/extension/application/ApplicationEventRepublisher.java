167e258ae1e252e612911861e8a7283db76dfc86
Johannes Graf <graf.johannes@gmail.com>
2023-07-19 20:44:40 +0200

    fixup! add ApplicationEventRepublisher to republish ApplicationAllowedEvents

--- src/main/java/org/synyx/urlaubsverwaltung/extension/application/ApplicationEventRepublisher.java
+++ src/main/java/org/synyx/urlaubsverwaltung/extension/application/ApplicationEventRepublisher.java
@@ -11,24 +11,22 @@ import org.springframework.stereotype.Component;
 import org.synyx.urlaubsverwaltung.application.application.ApplicationAllowedEvent;
 import org.synyx.urlaubsverwaltung.application.application.ApplicationService;
 import org.synyx.urlaubsverwaltung.application.application.ApplicationStatus;
-import org.synyx.urlaubsverwaltung.extension.sicknote.SickNoteEventHandlerExtension;
-import org.synyx.urlaubsverwaltung.sicknote.sicknote.SickNoteCreatedEvent;
 
 import java.time.LocalDate;
 
 @Component
 @ConditionalOnProperty(value = "uv.extensions.application.republish.enabled", havingValue = "true")
-@ConditionalOnBean(SickNoteEventHandlerExtension.class)
+@ConditionalOnBean(ApplicationEventHandlerExtension.class)
 public class ApplicationEventRepublisher {
 
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationEventRepublisher.class);
 
     private final ApplicationService applicationService;
-    private final ApplicationEventHandlerExtension sickNoteEventHandlerExtension;
+    private final ApplicationEventHandlerExtension applicationEventHandlerExtension;
 
-    public ApplicationEventRepublisher(ApplicationService applicationService, ApplicationEventHandlerExtension sickNoteEventHandlerExtension) {
+    public ApplicationEventRepublisher(ApplicationService applicationService, ApplicationEventHandlerExtension applicationEventHandlerExtension) {
         this.applicationService = applicationService;
-        this.sickNoteEventHandlerExtension = sickNoteEventHandlerExtension;
+        this.applicationEventHandlerExtension = applicationEventHandlerExtension;
     }
 
     @Async
@@ -47,7 +45,7 @@ public class ApplicationEventRepublisher {
             .map(ApplicationAllowedEvent::of)
             .forEach(event -> {
                 LOG.info("Publishing ApplicationAllowedEvent with id={} for personId={} with startDate={} and endDate={}", event.getApplication().getId(), event.getApplication().getPerson().getUsername(), event.getApplication().getStartDate(), event.getApplication().getEndDate());
-                sickNoteEventHandlerExtension.on(event);
+                applicationEventHandlerExtension.on(event);
             });
 
         LOG.info("Republished all events with type=ApplicationAllowedEvent");
